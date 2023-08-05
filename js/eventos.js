a84dfedcc3a96002e626bc1d0f7011780c32f46a
Juan Antelo <juancantelo23@gmail.com>
2023-08-05 13:38:10 -0300

    solucion click
    evitar el bubbling al div

--- js/eventos.js
+++ js/eventos.js
@@ -2,4 +2,7 @@
         document.getElementById("container").addEventListener("click", function(){
             alert("Hola! Soy el div");
         });
+        document.getElementById("boton").addEventListener("click", (e) =>{
+            e.stopPropagation();
+        })
     });
